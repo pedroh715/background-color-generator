5ab559778d06c96164a03937a4fe34e834a8181b
Pedro Henrique Grandizoli <pedrohenriquecontato04@gmail.com>
2021-11-25 19:20:15 -0400

    adicionando-codigo

--- colorGen/script.js
+++ colorGen/script.js
@@ -1,1 +1,15 @@
-console.log('teste')
+const codHex = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const generateColor = document.querySelector('.btn-gen')
+let newColor = []
+
+generateColor.addEventListener('click', function() {
+    for (let i = 0; i <= 5; i++) {
+        const random = Math.floor(Math.random() * codHex.length)
+        newColor[i] = [codHex[random]]
+    }
+    let newColorHex = newColor.join('')
+    document.body.style.backgroundColor = '#' + newColorHex
+    
+    let hexInHTML = document.querySelector('.colorHex')
+    hexInHTML.textContent = '#' + newColorHex
+})
